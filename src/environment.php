e1559bfa288bd0454c5c511625ab5ee2467caebb
Stack Team <stackteam.official@gmail.com>
2023-01-18 16:09:24 +0330

    some chnage on env

--- src/environment.php
+++ src/environment.php
@@ -6,8 +6,16 @@ if (!file_exists(__DIR__ . '/../')) {
     throw new Exception(".env file is not exist");
 }
 
+//print_r($_SERVER);die();
+$platforms = require_once ('platforms.php');
+$platform = isset($platforms[$_SERVER['HTTP_HOST']]) ? isset($platforms[$_SERVER['HTTP_HOST']]) : null;
+
+if (is_null($platform)) {
+    throw new Exception("platform not found");
+}
+
 // Looing for .env at the root directory
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../envs/' . $platform);
 $dotenv->load();
 
 
---
+++ src/platforms.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+
+    'stackjob.ir' => 'stackjob',
+    'stackmag.ir' => 'stackmag',
+    'stackacademy.ir' => 'stackacademy',
+    'stackcloud.ir' => 'stackcloud',
+    'stackinvest.ir' => 'stackinvest',
+    'stacktalk.ir' => 'stacktalk',
+    'stackecosystem.ir' => 'stackecosystem'
+];
