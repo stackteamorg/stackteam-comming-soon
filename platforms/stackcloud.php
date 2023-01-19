c6c3d3294a22c574f66a5c44c557bdf3dd0b912f
Stack Team <stackteam.official@gmail.com>
2023-01-19 14:27:55 +0330

    change title in stackcloud

--- platforms/stackcloud.php
+++ platforms/stackcloud.php
@@ -5,6 +5,6 @@ return [
     'domain' => 'stackcloud.ir',
     'url' => 'https://stackcloud.ir/',
     'platform' => 'Stackcloud',
-    'title' => 'استک کلاد',
+    'title' => 'سکوی ابری استک‌کلاد',
     'description' => 'استک کلاد یک پلتفورم ابری ارائه دهنده خدمات سکوی ابری PaaS DBaaS LaaS CDN و ابجکت استوریج است'
 ];
