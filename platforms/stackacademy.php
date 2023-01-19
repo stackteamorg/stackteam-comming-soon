aef73b401172557a588f543430c5c2d142dbcab0
Stack Team <stackteam.official@gmail.com>
2023-01-19 14:23:31 +0330

    add stackacademy data

--- platforms/stackacademy.php
+++ platforms/stackacademy.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'domain' => '',
-    'url' => '',
-    'platform' => '',
-    'title' => '',
-    'description' => ''
+    'domain' => 'stackacademy.ir',
+    'url' => 'https://stackacademy.ir/',
+    'platform' => 'Stackacademy',
+    'title' => 'پلتفورم آموزش آنلاین استک‌آکادمی',
+    'description' => 'استک آکادمی یک پلتفورم آموزش آنلاین در حوزه برنامه نویسی، تکنولوژی و استارتاپ است'
 ];
